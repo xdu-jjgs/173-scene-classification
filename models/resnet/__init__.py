d53865275f66704401a64ca1f8117a605e2884e5
Unqua <332755351@qq.com>
2022-06-14 18:43:37 +0800

    update config

--- models/resnet/__init__.py
+++ models/resnet/__init__.py
@@ -3,3 +3,17 @@ from .resnet import ResNet
 __all__ = [
     'ResNet'
 ]
+
+'''
+python train.py configs/sar_msi/resnet18_3090.yaml ^
+--path ./runs/sar_msi/resnet18-train ^
+--no-validate ^
+--nodes 1 ^
+--gpus 1 ^
+--rank-node 0 ^
+--backend gloo ^
+--master-ip localhost ^
+--master-port 8888 ^
+--seed 30 ^
+--opt-level O0
+'''
