dc1803d63d8258dc38c1ca22e7f52f3ef7471dbf
Unqua <332755351@qq.com>
2022-06-14 21:55:05 +0800

    update test shell

--- models/resnet/__init__.py
+++ models/resnet/__init__.py
@@ -6,14 +6,18 @@ __all__ = [
 
 '''
 python train.py configs/sar_msi/resnet18_3090.yaml ^
---path ./runs/sar_msi/resnet18-train ^
---no-validate ^
---nodes 1 ^
---gpus 2 ^
---rank-node 0 ^
---backend gloo ^
---master-ip localhost ^
---master-port 8888 ^
---seed 30 ^
---opt-level O0
+        --path ./runs/sar_msi/resnet18-train ^
+        --no-validate ^
+        --nodes 1 ^
+        --gpus 2 ^
+        --rank-node 0 ^
+        --backend gloo ^
+        --master-ip localhost ^
+        --master-port 8888 ^
+        --seed 30 ^
+        --opt-level O0
+
+python test.py runs/resnet18-train/config.yaml ^
+        runs/resnet18-train/best.pth ^
+        --path runs/resnet18-test --device cuda:0,1
 '''
